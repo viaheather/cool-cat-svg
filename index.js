15a04f76d1fb41f27c6ad30c4dae2fc62c04fc4f
viaheather <viaheatherr@gmail.com>
2023-09-30 13:34:49 -0700

    fixed shapes file

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+const fs = require('fs');
 const prompt = require('./lib/prompt.js');
+const generateSVG = require('./lib/generateSVG.js')
+const { Triangle, Square, Circle } = require("./lib/shapes");
