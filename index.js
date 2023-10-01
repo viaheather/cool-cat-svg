c51dae05ed3c0a19170a51d942745b5fc27d23bc
viaheather <viaheatherr@gmail.com>
2023-09-30 19:13:35 -0700

    pushing something

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const promptUser = (answers) => {
             name: 'initials',
         }, {
             type: 'input',
-            message: 'What color do you want the logo to be in?',
+            message: 'What color do you want the logo to be in? Can be color keyword or hex color.',
             name: 'color',
         }, {
             type: 'list',
@@ -21,21 +21,21 @@ const promptUser = (answers) => {
     ])
 }
 
-const generateSVG = ({ initials, color, shape }) =>
-`
-<svg>
-${shape} ${color} ${initials} />;
-</svg>`
-    ;
+const generateSVG = ({ initials,color,shape }) => 
+    `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    ${this.shape}`
 
 // If answer length is less than 3 characters, then logo.svg will be written from the user answers
 const init = () => {
     promptUser()
         .then((answers) => {
-            if (answers.initials.length > 3) {
+            if (answers.initials.length > 3 || answers.initials.length === 0) {
                 console.log("Your initials must be 2-3 characters max.");
+            } else if (answers.color.length == 0 ){
+                console.log("Empty inputs.");
             } else {
-                writeFile('logo.svg', generateSVG(answers))
+                writeFile('logo.svg', generateSVG(answers));
+                console.log('Your logo is generated.');
             }
         })
         .catch((err) => console.error(err));
